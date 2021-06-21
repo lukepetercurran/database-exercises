e468f48e22d69f243cce9add0ddb2a1740a0d7dd
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-21 12:04:04 -0500

    morning push

---
+++ where_exercises.sql
@@ -0,0 +1,24 @@
+# part 1
+
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ;
+
+SELECT * FROM employees WHERE last_name like 'E%';
+
+SELECT * from employees where last_name like '%Q%';
+
+# part 2
+
+SELECT first_name FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+SELECT first_name FROM employees WHERE first_name = 'Irena' AND gender = 'M' OR first_name = 'Vidya' AND gender = 'M' OR first_name = 'Maya' AND gender = 'M'; #sloppy
+
+SELECT first_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'; #cleaner, but the part 1 solution with "AND gender = 'M';" tacked on is best
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+#SELECT * FROM employees WHERE last_name LIKE 'E%e'; #very nice.
+
+SELECT * FROM employees WHERE last_name LIKE '%Q%' and last_name NOT LIKE '%QU%';
+
