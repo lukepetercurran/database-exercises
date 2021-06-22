76f110bb15db5285c1f944976fde68759d8bed07
Luke Curran <luke.peter.curran54@gmail.com>
2021-06-22 15:10:15 -0500

    mid afternoon

--- indexes.sql
+++ indexes.sql
@@ -15,4 +15,19 @@ ALTER TABLE  salaries ADD INDEX salary_index (salary);
 
 USE codeup_test_db;
 
-CREATE TABLE quotes
+CREATE TABLE quotes (
+    id INT NOT NULL AUTO_INCREMENT,
+    content VARCHAR (240) NOT NULL,
+    AUTHOR VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE quotes ADD UNIQUE (content);
+
+INSERT INTO quotes  (content, author)
+VALUES ('The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.', 'Marcel Proust');
+
+# the following now generates an error message
+# ERROR 1062 (23000): Duplicate entry 'The real voyahe of discovery consists no seeking new landsca' for key 'quotes.content'
+INSERT INTO quotes  (content, author)
+VALUES ('The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.', 'Rando on Twitter');
